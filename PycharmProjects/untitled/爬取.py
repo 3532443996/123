37fe0ba343fa7dd1e7bfb5605528ef40ef1a45e1
3532443996 <3532443996@qq.com>
2017-10-19 13:54:51 +0800

    test

---
+++ PycharmProjects/untitled/爬取.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+import urllib.request
+
+
+def getHtml(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    return html
+
+
+html = getHtml('http://www.baidu.com/')
+
+print(html)
+
